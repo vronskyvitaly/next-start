9b7e26d5bc1fea74e9afff00db95eb66a4eaff2f
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-03-18 18:00:58 +0300

    feat: add visual page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import s from './page.module.scss'
+import { CardsSection } from '@components/sections/cards-section'
 
 export default async function HomePage() {
   return (
     <article className={s.root}>
-      <h1 style={{ fontSize: '40px', fontWeight: 800 }}>Home page</h1>
+      <CardsSection />
     </article>
   )
 }
