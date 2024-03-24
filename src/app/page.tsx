5916eebf16c370f5b35751593711a6fb1a7a28f5
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-03-24 12:47:24 +0300

    fix: removed unused styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import s from './page.module.scss'
 import { BanerSection } from '@/components/sections/baner-section'
 import { CardsSection } from '@/components/sections/cards-section'
 
 export default async function HomePage() {
   return (
-    <article className={s.root}>
+    <article>
       <BanerSection />
       <CardsSection />
     </article>
