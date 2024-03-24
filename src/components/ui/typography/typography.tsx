1b29467306622731bf022a363300bfeb0591e6f4
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-03-24 11:51:47 +0300

    feat: add typography component

---
+++ src/components/ui/typography/typography.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import cn from 'classnames'
+
+import s from './typography.module.scss'
+import { TypographyVariant } from '../../common/enums/enums'
+
+export type TypographyProps<T extends ElementType = 'p'> = {
+  as?: T
+  variant?: TypographyVariant
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType = 'p'>({
+  variant = TypographyVariant.P,
+  className,
+  as,
+  ...restProps
+}: TypographyProps<T> &
+  Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>) => {
+  const Component = as || 'p'
+
+  const typographyClasses = cn(s[variant], className)
+
+  return <Component className={typographyClasses} {...restProps} />
+}
