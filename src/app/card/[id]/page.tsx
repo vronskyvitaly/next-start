c1937e806aa98562fda55e4f3f05e680f1dbc949
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-03-25 08:06:36 +0300

    feat: add default Card page

---
+++ src/app/card/[id]/page.tsx
@@ -0,0 +1,13 @@
+import { fetchCard } from './action'
+import { Card } from '@/app/api/cards/type'
+
+export default async function CardPage({ params }: { params: { id: string } }) {
+  const card: Card = await fetchCard(params.id)
+  // console.log(card)
+
+  return (
+    <article>
+      <h1>{card.title}</h1>
+    </article>
+  )
+}
