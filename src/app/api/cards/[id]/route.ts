4f9b67c889bf8e95017c0aff72fc9c4f14ddb0ef
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-03-25 08:04:18 +0300

    feat: add fetch card route

---
+++ src/app/api/cards/[id]/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { unstable_noStore as noStore } from 'next/dist/server/web/spec-extension/unstable-no-store'
+import { connectToDB } from '@/utils/db'
+import Card from '@/models/card'
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  noStore()
+  try {
+    await connectToDB()
+
+    // Receiving a card from the server by id
+    const card = await Card.findById(params.id)
+
+    return NextResponse.json(card)
+  } catch (error) {
+    console.error('Error fetch card:', error)
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
