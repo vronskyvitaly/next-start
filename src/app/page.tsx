e62d9d790b55063ddb6415793558d95214d6ec63
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-04 09:15:51 +0300

    refactor: changed the structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import { BanerSection } from '@/components/sections/baner-section'
 import { CardsSection } from '@/components/sections/cards-section'
 import { Card } from '@/app/api/cards/type'
 import { fetchCards } from '@/components/sections/cards-section/actions'
+import { Header } from '@/components'
 
 export default async function HomePage() {
   const cards: Card[] = await fetchCards()
   return (
     <article>
+      <Header cards={cards} />
       <BanerSection />
-      <CardsSection cards={cards} />
+      <CardsSection />
     </article>
   )
 }
