03d4a86f9c40ab6bba75e7dc3559ecdcdae9918a
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-11 10:31:18 +0300

    feat: added title section

--- src/app/(pages)/basket/page.tsx
+++ src/app/(pages)/basket/page.tsx
@@ -1,10 +1,16 @@
 import s from './page.module.scss'
+import { BasketCards } from '@/components/sections/basket-cards-section/basket-cards'
+import { Typography } from '@/components/ui/typography/typography'
+import { TypographyVariant } from '@/common/enums'
 
 export default function BasketPage() {
   return (
     <article className={s.root}>
       <div className={s.container}>
-        <div className={s.flexPage}></div>
+        <Typography variant={TypographyVariant.H3} as={'h3'}>
+          Корзина
+        </Typography>
+        <BasketCards />
       </div>
     </article>
   )
