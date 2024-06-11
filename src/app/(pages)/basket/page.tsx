831a71007081ad146e1e9b5655d6164c8f6b1b1c
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-11 08:25:30 +0300

    feat: added markup and styles for the pag

--- src/app/(pages)/basket/page.tsx
+++ src/app/(pages)/basket/page.tsx
@@ -1,5 +1,11 @@
 import s from './page.module.scss'
 
 export default function BasketPage() {
-  return <article className={s.root}></article>
+  return (
+    <article className={s.root}>
+      <div className={s.container}>
+        <div className={s.flexPage}></div>
+      </div>
+    </article>
+  )
 }
