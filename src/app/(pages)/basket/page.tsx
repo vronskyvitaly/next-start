21b7f75365287f8b705e3d6ed262f95a79994363
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-11 08:12:10 +0300

    refactor

--- src/app/(pages)/basket/page.tsx
+++ src/app/(pages)/basket/page.tsx
@@ -1,46 +1,5 @@
-'use client'
 import s from './page.module.scss'
 
-import { useAppDispatch, useAppSelector } from '@/common/hooks'
-import {
-  decrementCount,
-  incrementCount,
-  resetValueCount,
-  setCount
-} from '@/lib/features/counter-slice'
-
 export default function BasketPage() {
-  const count = useAppSelector(setCount)
-  const dispatch = useAppDispatch()
-
-  const handleReset = () => {
-    dispatch(resetValueCount()) // Вызываем действие resetValueCount для сброса значения счетчика
-  }
-
-  return (
-    <article className={s.root}>
-      <h1>Корзина</h1>
-      <h2>{count}</h2>
-      <div style={{ display: 'flex', gap: '20px' }}>
-        <button
-          style={{ padding: '10px', borderRadius: '30px' }}
-          onClick={() => dispatch(incrementCount())}
-        >
-          +
-        </button>
-        <button
-          style={{ padding: '10px', borderRadius: '30px' }}
-          onClick={() => dispatch(decrementCount())}
-        >
-          -
-        </button>
-        <button
-          style={{ padding: '10px', borderRadius: '30px' }}
-          onClick={handleReset}
-        >
-          Reset
-        </button>
-      </div>
-    </article>
-  )
+  return <article className={s.root}></article>
 }
