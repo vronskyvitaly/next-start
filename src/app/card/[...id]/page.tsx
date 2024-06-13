92d753824a0275cbaca966b643424b2a85577139
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-13 08:37:13 +0300

    fix 6

--- src/app/card/[...id]/page.tsx
+++ src/app/card/[...id]/page.tsx
@@ -2,6 +2,7 @@ import { fetchCard } from './actions'
 import { Card } from '@/app/api/cards/type'
 
 export const dynamic = 'force-static'
+export const dynamicParams = true
 
 export default async function CardPage({ params }: { params: { id: string } }) {
   const card: Card = await fetchCard(params.id)
--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -76,7 +76,7 @@ export const Header = ({ cards }: HeaderProps) => {
               <Link href={'/basket'} className={s.link}>
                 <IconWrapper
                   counter={getBasketItemCount()}
-                  subTitle={'Корзина5'}
+                  subTitle={'Корзина6'}
                   srcImg={
                     'https://static.vecteezy.com/system/resources/thumbnails/004/798/846/small/shopping-cart-logo-or-icon-design-vector.jpg'
                   }
