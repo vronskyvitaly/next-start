e5896a7e40c5baeb62dee1a3ad48f4e80a26516d
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-13 19:48:05 +0300

    feat: added metadata to the card page

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -1,6 +1,13 @@
 import { CardSection } from '@/components/sections/card-section/card-section'
 import { fetchCards } from '@/components/sections/cards-section/actions'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Card page',
+  description: 'Card page',
+  applicationName: 'Shop',
+  creator: 'Vronsky Vitaly'
+}
 export default async function Page() {
   const cards = await fetchCards()
   return (
