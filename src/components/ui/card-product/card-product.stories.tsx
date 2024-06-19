2d3ae66d4922e2f808e9002f8979a0c323582939
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-19 21:02:36 +0300

    feat: added stories for different states

---
+++ src/components/ui/card-product/card-product.stories.tsx
@@ -0,0 +1,71 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { CardProduct } from '@componentsUI/*'
+import StoreProvider from '@app/store-provider'
+
+const meta: Meta<typeof CardProduct> = {
+  title: 'Components/CardProducts',
+  component: CardProduct,
+  tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    price: 2000,
+    discount: 500,
+    title: 'Product 1'
+  },
+
+  render: args => (
+    <StoreProvider>
+      <CardProduct {...args} />
+    </StoreProvider>
+  )
+}
+
+export const DefaultCardInTheBasket: Story = {
+  args: {
+    price: 2000,
+    discount: 500,
+    title: 'Product 1',
+    cardInTheBasket: true
+  },
+
+  render: args => (
+    <StoreProvider>
+      <CardProduct {...args} />
+    </StoreProvider>
+  )
+}
+
+export const DefaultCardInTheFavorites: Story = {
+  args: {
+    price: 2000,
+    discount: 500,
+    title: 'Product 1',
+    isFavorites: true
+  },
+
+  render: args => (
+    <StoreProvider>
+      <CardProduct {...args} />
+    </StoreProvider>
+  )
+}
+
+export const DefaultBasketWithCounter: Story = {
+  args: {
+    price: 2000,
+    discount: 500,
+    title: 'Product 1',
+    variants: 'inBasketAndCounter'
+  },
+
+  render: args => (
+    <StoreProvider>
+      <CardProduct {...args} />
+    </StoreProvider>
+  )
+}
