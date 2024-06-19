1016a491d4690a8432b660b0375c0b126d6f436f
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-06-19 18:07:59 +0300

    refactor: import card-product

--- src/components/sections/cards-section/cards-section.tsx
+++ src/components/sections/cards-section/cards-section.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import s from './cards.section.module.scss'
-import { Section } from '@componentsUI/*'
+import { Section, CardProduct } from '@componentsUI/*'
 import React, { CSSProperties } from 'react'
 import { setBasketCardsSelector } from '@/lib/features/basket-slice'
-import { CardProduct } from '@components/sections/cards-section/card-product'
 import { useAppSelector } from '@common/hooks'
 
 type Props = {
